3654b325b475c04cfcf3843b818f601fc1906b89
pabloV <vega_perezpablo@hotmail.com>
2026-06-02 15:31:34 -0600

    changed argv in main

--- src/main.py
+++ src/main.py
@@ -2,12 +2,11 @@ from copystatic import copy_static_into_public
 from gencontent import generate_pages_recursive
 import sys
 
-if sys.argv[1]:
+if len(sys.argv) >= 2 and sys.argv[1] == "/staticSite":
     base_path = sys.argv[1]
 else:
     base_path = "/"
-
-
+    
 copy_static_into_public()
 
 generate_pages_recursive("content", "template.html", "docs", base_path)
